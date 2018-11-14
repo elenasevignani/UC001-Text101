a9450245a5113311820c5110639e53152c9cb35d
elenasevignani <elena.sevignani@htl-salzburg.ac.at>
2018-11-14 14:36:35 +0100

    test methods

--- Assets/MyGame/Scripts/RandomState.cs
+++ Assets/MyGame/Scripts/RandomState.cs
@@ -2,6 +2,11 @@
 
 
 public class RandomState{
-    public static readonly Random getrandom = new Random(123);
-	
+    public readonly Random getrandom = new Random(123);
+    public RandomState (){
+
+    }
+    public int getNext (int min, int max){
+        return getrandom.Next(min, max);
+    }
 }
